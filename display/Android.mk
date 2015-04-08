c238ee4e42420b04f6e4d10c044f90e96a34c8cc
myfluxi <linflux@arcor.de>
2015-04-08 18:54:49 +0200

    hammerheadcaf: Update display HAL
    https://github.com/CyanogenMod/android_hardware_qcom_display
    commit: 2d44d682e8a7dd3bd190bf7f18b19c0655425c41
    
    Change-Id: I122eb7df414e351975e3e3d7818ae9b9d6080833

--- display/Android.mk
+++ display/Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(TARGET_DEVICE),hammerheadcaf)
 
 display-hals := libgralloc libgenlock libcopybit libvirtual
-display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
 display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
