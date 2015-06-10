322abaa34d8f6f7a12d55130ac152fcfb5c36aaf
Ronghua Wu <ronghuawu@google.com>
2015-06-10 11:32:44 -0700

    hammerhead: add media_codecs_performance.xml
    Bug: 22504214
    Change-Id: I456f956622fa5d01d4e81ec5376c4ee1b538c31d

--- device.mk
+++ device.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
