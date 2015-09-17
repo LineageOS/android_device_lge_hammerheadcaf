cc3e58fdda9941025f95117dbdb4c95e4a3e94d2
myfluxi <linflux@arcor.de>
2015-09-17 12:26:18 +0200

    hammerheadcaf: power: Set I/O scheduler by system property
    Change-Id: Ieab0434b179a1df6b6e93d4537db7594d9d79083

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
+# I/O Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=cfq
+
 # QCOM Perf lib
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
