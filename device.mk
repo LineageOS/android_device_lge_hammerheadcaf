6bbfe503350efeeb16d03bc17bf7ab43c4157fb8
Raghavendra Prasad Nagaraj <prasr@codeaurora.org>
2015-11-23 15:41:33 +0530

    msm8974: Change heapminfree
    Make this parameter's value in line with legacy.
    This will improve first launch latencies.
    
    Change-Id: Ia7ad5c88cfa0bbbc1a32b34f0be406e06319655c

--- device.mk
+++ device.mk
@@ -370,7 +370,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.input.noresample=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-swap=false
+    dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.heapminfree=2m
 
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
