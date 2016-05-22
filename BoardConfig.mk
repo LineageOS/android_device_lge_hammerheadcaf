c8f119bc71e085cb9c74b7a663defeabd30f805b
fluxi <linflux@arcor.de>
2016-05-22 19:43:14 +0200

    Revert "hammerheadcaf: Build init vendor lib"
    Adding a local lib if we need it.
    
    This reverts commit b0e3541c1660a9adf07c19fc8553c70625525df4.
    
    Change-Id: I91ae2719944033babdb7dc1228af7da11b459b4d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,9 +109,6 @@ COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
-
 TARGET_RECOVERY_FSTAB = device/lge/hammerheadcaf/fstab.hammerhead
 
 # Releasetools
