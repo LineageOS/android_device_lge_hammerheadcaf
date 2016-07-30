a309e487cea8c593c4d5e1246beae897defc2845
fluxi <linflux@arcor.de>
2016-07-30 19:25:33 +0200

    hammerheadcaf: Enable sdcardfs
    Change-Id: I08fa1bd6ff4b6fda8cac445f8d2b62b6b55d7738

--- device.mk
+++ device.mk
@@ -361,6 +361,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false \
     dalvik.vm.heapminfree=2m
 
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sdcardfs.enable=true
+
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
