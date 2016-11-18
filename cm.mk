43e15bd7b14ddea9fa0f1754b800992385c5ddac
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 02:36:33 -0700

    hammerheadcaf: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8
    (cherry picked from commit 0d84f601978a9815e628c6d8ccc0f75b27cd29f6)

--- cm.mk
+++ cm.mk
@@ -20,9 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerheadcaf/device.mk)
 
